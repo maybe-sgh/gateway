e821d5e5cd6a40bb68fd98f0ab8341499072553f
liv <1171914002@qq.com>
2020-12-21 18:18:35 +0800

    gateway代码  add by suguanhua

---
+++ java/com/bocins/springcloudgateway/SpringcloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bocins.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringcloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGatewayApplication.class, args);
+    }
+}
